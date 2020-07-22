2a3ee2242558be1219d2695233933ecf83eba1d9
Angel Angelov <ang.k.angelov@gmail.com>
2020-07-22 18:33:02 +0300

    feat: correct/incorrect logic implemented on answer

--- my-app/src/components/questions/Answer.jsx
+++ my-app/src/components/questions/Answer.jsx
@@ -3,10 +3,21 @@ import styles from './Answer.module.css'
 
 class Answer extends React.Component {
 
+    handleClick = (e) => {
+        if (e.currentTarget.innerText === this.props.correctAnswer) {
+            console.log("correct")
+        } else {
+            console.log("not correct")
+        }
+        // console.log(e.currentTarget.innerText);
+        // console.log(this.props.correctAnswer);
+    }
+
     render() {
 
     return(
         <>
+        <p>{this.props.correctAnswer}</p>
         <h3>Jokers</h3>
         <section className={styles.jokersContainer}>    
             <button className={styles.jokerItem}>Call a friend</button>
@@ -15,10 +26,10 @@ class Answer extends React.Component {
         </section>
         <h3>{this.props.question}</h3>
         <section className={styles.answersContainer}>
-            <button className={styles.answerItem}>{this.props.answerA}</button>
-            <button className={styles.answerItem}>{this.props.answerB}</button>
-            <button className={styles.answerItem}>{this.props.answerC}</button>
-            <button className={styles.answerItem}>{this.props.answerD}</button>
+            <button onClick={this.handleClick} className={styles.answerItem}>{this.props.answerA}</button>
+            <button onClick={this.handleClick} className={styles.answerItem}>{this.props.answerB}</button>
+            <button onClick={this.handleClick} className={styles.answerItem}>{this.props.answerC}</button>
+            <button onClick={this.handleClick} className={styles.answerItem}>{this.props.answerD}</button>
         </section>
         </>
     )
