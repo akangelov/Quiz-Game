afc3658112c1b063c2dddec5c208dcf2ba22401d
Angel Angelov <ang.k.angelov@gmail.com>
2020-07-26 14:14:54 +0300

    feat:cloudinary integration

--- my-app/src/components/questions/Category.jsx
+++ my-app/src/components/questions/Category.jsx
@@ -6,14 +6,26 @@ const Category = () => {
 
     return (
         <>
-        <h2>Please choose a category</h2>
-          <section className = {styles.categoryContainer}>
-            <Link className={styles.categoryItem} to="/questions/biology">Biology</Link>
-            <Link className={styles.categoryItem} to="/questions/geography">Geography</Link>
-            <Link className={styles.categoryItem} to="/questions/history">History</Link>
-            <Link className={styles.categoryItem} to="/questions/science">Science</Link>
+        <h2>Please choose a category:</h2>
+        <section className = {styles.categoryContainer}>
+          <div className = {styles.imageContainer}>
+              <img className= {styles.Image} src ="https://res.cloudinary.com/dq336pwqs/image/upload/v1595758389/biology_yt6fju.jpg" alt="biology"/>
+              <Link className={styles.categoryItem} to="/questions/biology">Biology</Link>
+          </div>
+          <div className = {styles.imageContainer}>
+              <img src ="https://res.cloudinary.com/dq336pwqs/image/upload/v1595757954/history_ptn99s.jpg" alt="history"/>
+              <Link className={styles.categoryItem} to="/questions/history">History</Link>
+          </div>
+          <div className = {styles.imageContainer}>
+              <img src ="https://res.cloudinary.com/dq336pwqs/image/upload/v1595757954/geography_dbevsx.jpg" alt="geography"/>
+              <Link className={styles.categoryItem} to="/questions/geography">Geography</Link>
+          </div>
+          <div className = {styles.imageContainer}>
+              <img src ="https://res.cloudinary.com/dq336pwqs/image/upload/c_scale,w_750/v1595759504/science_i5fmz.jpg" alt="science" />
+              <Link className={styles.categoryItem} to="/questions/science">Science</Link>
+          </div>
           </section>
-        </>  
+        </>    
     )
 }
 
