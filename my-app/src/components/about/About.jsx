20cefdd190153cc499b53b506dd2c355587c0d76
Angel Angelov <ang.k.angelov@gmail.com>
2020-07-30 11:27:54 +0300

    css update

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -5,11 +5,10 @@ const About = () => {
     return (
         <>
         <div className={styles.about}>
-            <p>Quiz game is an interactive game that checks your knowledge
+            <p className={styles.aboutP}>Quiz game is an interactive game that checks your knowledge
                 in History, Biology, Math or Science.</p>
             <img src ="https://res.cloudinary.com/dq336pwqs/image/upload/c_scale,w_460/v1596096430/quiz.jpg" alt="quiz"/>         
-        </div> 
-     
+        </div>     
         </>
     )
 };
