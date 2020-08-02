05e8c60076ceb0365403afcd02200acf4b8406f8
Angel Angelov <ang.k.angelov@gmail.com>
2020-08-02 15:38:31 +0300

    userprof:pics added

--- my-app/src/pages/user/UserProfile.jsx
+++ my-app/src/pages/user/UserProfile.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import UserContext from '../../utils/Context';
+import styles from './UserProfile.module.css'
 
 const UserProfile = props => {
    
     const value = React.useContext(UserContext);
-    // console.log(value)
     
+
         return (
+            <>
+            <div className = {styles.imageContainer}>
+                <img className= {styles.Image} src ="https://res.cloudinary.com/dq336pwqs/image/upload/c_scale,w_366/v1596370427/true.jpg" alt="true"/>
+            </div>
+            <div className = {styles.imageContainer}>
+                <img className= {styles.Image} src ="https://res.cloudinary.com/dq336pwqs/image/upload/c_scale,w_366/v1596371800/false1.jpg" alt=""/>
+            </div>
             <div>
                 <p>User: {value.user.username}</p>
-                <p>Score: {value.user.score}</p>
+                <p>Correct Answers: {value.user.score}</p>
                 <p>Wrong Answers: {value.user.wrongAnswers}</p>
             </div>
+            </>
         )
 }
 
