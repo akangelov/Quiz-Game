ebc223f43987a2563be419d110d71e8967e5eb9c
Angel Angelov <ang.k.angelov@gmail.com>
2020-08-02 18:35:52 +0300

    images added

--- my-app/src/pages/user/UserProfile.jsx
+++ my-app/src/pages/user/UserProfile.jsx
@@ -9,16 +9,18 @@ const UserProfile = props => {
 
         return (
             <>
-            <div className = {styles.imageContainer}>
-                <img className= {styles.Image} src ="https://res.cloudinary.com/dq336pwqs/image/upload/c_scale,w_366/v1596370427/true.jpg" alt="true"/>
-            </div>
-            <div className = {styles.imageContainer}>
-                <img className= {styles.Image} src ="https://res.cloudinary.com/dq336pwqs/image/upload/c_scale,w_366/v1596371800/false1.jpg" alt=""/>
-            </div>
-            <div>
-                <p>User: {value.user.username}</p>
-                <p>Correct Answers: {value.user.score}</p>
-                <p>Wrong Answers: {value.user.wrongAnswers}</p>
+            <div className = {styles.container}>
+                <div className = {styles.imageContainer}>
+                    <p className={styles.categoryItem}>User: {value.user.username}</p>   
+                </div>
+                <div className = {styles.imageContainer}>
+                    <p className={`${styles.categoryItem} ${styles.correct}`}>Correct Answers: {value.user.score}</p>
+                    <img className= {styles.Image} src ="https://res.cloudinary.com/dq336pwqs/image/upload/v1596382490/true3.png" alt="correct"/>
+                </div>
+                <div className = {styles.imageContainer}>
+                    <p className={`${styles.categoryItem} ${styles.wrong}`}>Wrong Answers: {value.user.wrongAnswers}</p>
+                    <img className= {styles.Image} src ="https://res.cloudinary.com/dq336pwqs/image/upload/c_scale,w_366/v1596371800/false1.jpg" alt="wrong"/>
+                </div>  
             </div>
             </>
         )
