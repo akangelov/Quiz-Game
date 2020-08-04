3fc5e8b2a1bcea387dfe07659cdd3d03e92a2a5b
Angel Angelov <ang.k.angelov@gmail.com>
2020-08-04 15:24:36 +0300

    feat:not found

--- my-app/src/pages/notFound/PageNotFound.jsx
+++ my-app/src/pages/notFound/PageNotFound.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styles from "./PageNotFound.module.css"
 
 const PageNotFound = () => {
     return (
         <>
-        <p>Error222</p>
-        <Link to="/">Go to Home </Link>
+        <div >
+            <img className={styles.notFound} src ="https://res.cloudinary.com/dq336pwqs/image/upload/v1596543370/notFound.jpg" alt="notFound"/>
+        </div>
+        <Link className={styles.link} to="/"> Home </Link>
         </>
     )
 }
