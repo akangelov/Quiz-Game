24a4c210dc36fb8165c7d5f41dbc1a94acdd54e3
Angel Angelov <ang.k.angelov@gmail.com>
2020-09-01 11:00:32 +0300

    updates

--- rest/controllers/user.js
+++ rest/controllers/user.js
@@ -3,22 +3,22 @@ const config = require('../config/config');
 const utils = require('../utils');
 
 module.exports = {
-  get: (req, res, next) => {
-    const id = req.query.id
-    // console.log(id)
-    models.User.findById(id)
-      .then((user) => res.send(user))
-      .catch(next)
-  },
+    get: (req, res, next) => {
+        const id = req.query.id
+        models.User.findById(id)
+            .then((user) => res.send(user))
+            .catch(next)
+    },
 
-  post: {
+    post: {
+    
     register: (req, res, next) => {
-      const score = 0;  
-      const wrongAnswers = 0;
-      const { username, password } = req.body;
-      models.User.create({ username, password, score, wrongAnswers })
-        .then((createdUser) => res.send(createdUser))
-        .catch(next)
+        const score = 0;  
+        const wrongAnswers = 0;
+        const { username, password } = req.body;
+        models.User.create({ username, password, score, wrongAnswers })
+            .then((createdUser) => res.send(createdUser))
+            .catch(next)
     },
 
     verifyLogin: (req, res, next) => {
@@ -34,8 +34,8 @@ module.exports = {
                 models.User.findById(data.id)
                     .then((user) => {
                         return res.send({
-                          status: true,
-                          user
+                            status: true,
+                            user
                         })
                     });
             })
@@ -46,59 +46,52 @@ module.exports = {
                     res.status(401).send('UNAUTHORIZED!');
                     return;
                 }
-
-                res.send({
-                  status: false
-                })
+            res.send({
+              status: false
             })
-      },
+        })
+    },
 
     login: (req, res, next) => {
-      const { username, password } = req.body;
-      models.User.findOne({ username })
-        .then((user) => Promise.all([user, user.matchPassword(password)]))
-        .then(([user, match]) => {
-          if (!match) {
-            res.status(401).send('Invalid password');
-            return;
+        const { username, password } = req.body;
+        models.User.findOne({ username })
+            .then((user) => Promise.all([user, user.matchPassword(password)]))
+            .then(([user, match]) => {
+                if (!match) {
+                    res.status(401).send('Invalid password');
+                    return;
           }
 
-          const token = utils.jwt.createToken({ id: user._id });
-          res.cookie(config.authCookieName, token).send(user);
+        const token = utils.jwt.createToken({ id: user._id });
+        res.cookie(config.authCookieName, token).send(user);
         })
         .catch(next);
     },
 
     logout: (req, res, next) => {
-      const token = req.cookies[config.authCookieName];
-      // console.log('-'.repeat(100));
-      // console.log(token);
-      // console.log('-'.repeat(100));
-      models.TokenBlacklist.create({ token })
-        .then(() => {
-          res.clearCookie(config.authCookieName).send('Logout successfully!');
-        })
-        .catch(next);
+        const token = req.cookies[config.authCookieName];
+        models.TokenBlacklist.create({ token })
+            .then(() => {
+                res.clearCookie(config.authCookieName).send('Logout successfully!');
+            })
+            .catch(next);
     }
   },
 
-  put: (req, res, next) => {
-    const id = req.params.id;
-    // console.log(id)
-  //   console.log(req)
-  // const { username, password } = req.body;
-      const score = req.body.score
-      const wrongAnswers = req.body.wrongAnswers
-      console.log(score, wrongAnswers)
-      models.User.update({ _id: id }, { score, wrongAnswers })
-    .then((updatedUser) => res.send(updatedUser))
-    .catch(next)
-},
+    put: (req, res, next) => {
+        const id = req.params.id;
+        const score = req.body.score
+        const wrongAnswers = req.body.wrongAnswers
+        console.log(score, wrongAnswers)
+        models.User.update({ _id: id }, { score, wrongAnswers })
+            .then((updatedUser) => res.send(updatedUser))
+            .catch(next)
+    },
 
-  delete: (req, res, next) => {
-    const id = req.params.id;
-    models.User.deleteOne({ _id: id })
-      .then((removedUser) => res.send(removedUser))
-      .catch(next)
-  }
+    delete: (req, res, next) => {
+      const id = req.params.id;
+      models.User.deleteOne({ _id: id })
+          .then((removedUser) => res.send(removedUser))
+          .catch(next)
+    }
 };
