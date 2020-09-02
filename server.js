9fc98642b047ab737e64bf10e827971bb3268b86
Angel Angelov <ang.k.angelov@gmail.com>
2020-09-02 19:53:38 +0300

    update

---
+++ server.js
@@ -0,0 +1,32 @@
+const config = require('./Server/config/config');
+const dbConnection = require('./Server/config/database');
+
+const app = require('express')();
+
+dbConnection().then(() => {
+
+    require('./Server/config/express')(app);
+
+    require('./Server/config/routes')(app);
+
+    // app.use(function (err, req, res, next) {
+    //     console.error(err);
+    //     // res.status(500).send(err.message);
+    //     console.log('*'.repeat(90))
+    // });
+
+    // if(process.env.NODE_ENV === 'production') {
+    //     app.use(express.static('Client/build'));
+  
+    //     app.get('*', (req, res) => {
+    //         res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'))
+    //     })
+    // }
+
+    if(process.env.NODE_ENV === 'production') {
+        app.use(express.static('Client/build'));
+    }
+
+    app.listen(config.port, console.log(`Listening on port ${config.port}!`))
+
+}).catch(console.error);
