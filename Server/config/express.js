659f0816a864e285d0693ed243543edd1525470e
Angel Angelov <ang.k.angelov@gmail.com>
2020-09-02 21:18:44 +0300

    m

--- Server/config/express.js
+++ Server/config/express.js
@@ -5,10 +5,10 @@ const secret = 'secret';
 const path = require('path');
 
 module.exports = (app) => {
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-  }));
+//   app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true
+//   }));
 
   app.use(bodyParser.urlencoded({
     extended: true
