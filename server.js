c4a7b2213870bff12c968ed14881ef0f9c8dc4d6
Angel Angelov <ang.k.angelov@gmail.com>
2020-09-03 09:29:55 +0300

    m

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ dbConnection().then(() => {
     if(process.env.NODE_ENV === 'production') {
         app.use(express.static('Client/build'));
 
-            app.get('*', (req, res) => {
+            app.get('/about', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'))
+        })
+
+        app.get('/*', (req, res) => {
             res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'))
         })
     }
