e05fae7b4026b8ccf2107302f070fcf92b705a5a
Angel Angelov <ang.k.angelov@gmail.com>
2020-09-03 09:21:39 +0300

    m

--- Server/config/config.js
+++ Server/config/config.js
@@ -8,7 +8,7 @@ const config = {
         authCookieName: 'x-auth-token'
     },
     production: {
-        port: process.env.PORT || 5000,
+        port: process.env.PORT || 4999,
         dbURL: 'mongodb+srv://angel123:angel123@cluster0.gwqda.mongodb.net/myproject?retryWrites=true&w=majority',
         authCookieName: 'x-auth-token'
     }
