8d8df3f66ec098d33eadaea840216bc1587f4e04
Angel Angelov <ang.k.angelov@gmail.com>
2020-09-07 17:38:27 +0300

    add

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 
 const config = {
     development: {
@@ -8,9 +8,9 @@ const config = {
         authCookieName: 'x-auth-token'
     },
     production: {
-        // port: process.env.PORT || 9999,
-        // dbURL: 'mongodb+srv://angel123:angel123@cluster0.gwqda.mongodb.net/myproject?retryWrites=true&w=majority',
-        // authCookieName: 'x-auth-token'
+        port: process.env.PORT || 9999,
+        dbURL: 'mongodb+srv://angel123:angel123@cluster0.gwqda.mongodb.net/myproject?retryWrites=true&w=majority',
+        authCookieName: 'x-auth-token'
     }
 };
 
